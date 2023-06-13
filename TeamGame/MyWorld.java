c938d0ca0eab1aec83d2b14e5b4c78080b5318a3
FreddyKnapp <128709055+FreddyKnapp@users.noreply.github.com>
2023-06-13 09:22:20 -0400

    Fixed

--- TeamGame/MyWorld.java
+++ TeamGame/MyWorld.java
@@ -13,11 +13,9 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
-    {    
-       
-    private void prepare()
-    {
+    public MyWorld() {
+        super(600, 400, 1); 
+    } 
+    
 
-    }
 }
